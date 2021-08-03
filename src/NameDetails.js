2cbe280b9bed08a59e76d8275ebc9fb21e8de2ad
Ruben Aleman <rubenq3a@gmail.com>
2021-08-03 14:27:37 -0300

    Function component added but commented

--- src/NameDetails.js
+++ src/NameDetails.js
@@ -3,11 +3,47 @@ import React, { useRef } from "react";
 // Changing from Class Components to Function Components
 
 /*
+    
+function NameDetails(props) {
 
-function NameDetails() {
+    const [names, setNames] = useState([]);
+    const [buttonState, setButtonState] = useState(false)
+    const nameRef = useRef();
+
+    function getNameDetails() {
+        fetch('https://api.nationalize.io?name='+nameRef.current.value)
+          .then(res => res.json())
+          .then((json) => setNames({ names: json.country }));
+      }
+
+
+    const setButtonState = (e) => 
+    {
+        if (e.target.value.length >= 3)
+        {
+            buttonState = true;
+        }
+        else
+        {
+            buttonState = false;
+        }
+
+    }
+
     return(
         <>
-            
+            <input type="text" placeholder="Insert your name here" onChange={setButtonState} ref={nameRef}></input>
+            <button disable:{buttonState}>I want to know everything about my Name</button>
+            <ul>
+                {names.map((item) => {
+                    const country = 'https://www.countryflags.io/'+item.country_id+'/shiny/64.png'
+                    const roundedProb = Number((item.probability*100).toFixed(2));
+                    return <li>
+                        There are %{roundedProb} of chances that your name comes from {item.country_id}
+                        <img src={country} alt="Flag" width="25" height="25" style={imgStyle}></img>
+                        </li>
+                })}
+            </ul>
         </>
     )
 }
